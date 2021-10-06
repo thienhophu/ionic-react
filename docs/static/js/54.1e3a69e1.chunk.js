fdce536d1567a3cf253c0a79427df155250e35c7
Thien Ho <thien.hophu@gmail.com>
2021-10-06 21:45:16 +0700

    docs

---
+++ docs/static/js/54.1e3a69e1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpionic-react"]=this["webpackJsonpionic-react"]||[]).push([[54],{330:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(16),c=e(14),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var r=o.closest("ion-content");r&&new Promise((function(n){return Object(c.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=54.1e3a69e1.chunk.js.map
